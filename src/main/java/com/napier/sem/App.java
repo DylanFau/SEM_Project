4697a008a5dfa9f1335afdbeaf19443da61fb1a6
Dylan Faulkner <dylanfaulkner616@gmail.com>
2025-11-06 01:50:59 +0000

    Database integration complete. Setup to use MySQL using the docs. Query parsing could be improved, can be done in future.
    Signed-off-by: Dylan Faulkner <dylanfaulkner616@gmail.com>

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,9 @@
 package com.napier.sem;
 
+import java.sql.*;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Database_Connection.query("SELECT * FROM city LIMIT 5");
     }
 }
