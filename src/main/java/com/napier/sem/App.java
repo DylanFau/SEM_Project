f891a5c1916ee9cf1784e95e9dc194571f9a83dc
Dylan Faulkner <dylanfaulkner616@gmail.com>
2025-11-10 18:02:57 +0000

    Added some queries to give people working on them an idea of how to columns look in the tables
    Signed-off-by: Dylan Faulkner <dylanfaulkner616@gmail.com>

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,6 +4,18 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Database_Connection.query("SELECT * FROM city LIMIT 5");
+//        Database_Connection.query("SELECT * FROM city LIMIT 5"); // Use this boilerplate to run your queries.
+        checkDatabaseColumns();
+    }
+
+    public static void checkDatabaseColumns() throws SQLException, ClassNotFoundException {
+        System.out.println("Table 1: city");
+        Database_Connection.query("SELECT * FROM city LIMIT 2");
+
+        System.out.println("\nTable 2: country");
+        Database_Connection.query("SELECT * FROM country LIMIT 2");
+
+        System.out.println("\nTable 3: countrylanguage");
+        Database_Connection.query("SELECT * FROM countrylanguage LIMIT 2");
     }
 }
