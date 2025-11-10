936a7682cd066a1f218fddd4900ecc5456390a07
Dylan Faulkner <dylanfaulkner616@gmail.com>
2025-11-10 20:22:49 +0000

    Renamed test file, was not getting discovered by github actions
    Signed-off-by: Dylan Faulkner <dylanfaulkner616@gmail.com>

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,18 +1,17 @@
 package com.napier.sem;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MyTest {
+public class Test {
 
     // Simple test just to confirm JUnit is working
-    @Test
+    @org.junit.jupiter.api.Test
     void basicTest() {
         assertEquals(5, 5);
     }
 
     // Test that a database connection can be created
-    @Test
+    @org.junit.jupiter.api.Test
     void testDatabaseConnection() {
         try {
             var conn = Database_Connection.getConnection();
@@ -24,7 +23,7 @@ public class MyTest {
     }
 
     // Test that a query runs without errors
-    @Test
+    @org.junit.jupiter.api.Test
     void testQueryRuns() {
         try {
             Database_Connection.query("SELECT Name FROM country LIMIT 3;");
@@ -34,7 +33,7 @@ public class MyTest {
     }
 
     // Test: Query top 5 countries by population
-    @Test
+    @org.junit.jupiter.api.Test
     void testQueryTopCountries() {
         try {
             Database_Connection.query("SELECT Name, Population FROM country ORDER BY Population DESC LIMIT 5;");
@@ -44,7 +43,7 @@ public class MyTest {
     }
 
     // Test: Query average population of countries in each continent
-    @Test
+    @org.junit.jupiter.api.Test
     void testQueryAveragePopulationByContinent() {
         try {
             Database_Connection.query("SELECT Continent, AVG(Population) AS Average_Population FROM country GROUP BY Continent;");
